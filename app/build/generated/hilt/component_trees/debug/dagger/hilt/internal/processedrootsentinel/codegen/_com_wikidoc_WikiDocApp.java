2338b7c2ae05cd9abd62164279ee8f56d79c7942
WikiDoc Developer <developer@wikidoc.local>
2026-05-08 01:38:42 +0800

    chore: remove build directory from git tracking

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_wikidoc_WikiDocApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.wikidoc.WikiDocApp"
-)
-public final class _com_wikidoc_WikiDocApp {
-}
