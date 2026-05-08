ca8e24e3108c6bcdb1ccbeee2ae2f60b5935fedb
WikiDoc Developer <developer@wikidoc.local>
2026-05-08 23:07:35 +0800

    "fix: 调整拖拽预览卡片Y轴偏移为0，让触点与卡片左上角对齐"

--- app/src/main/java/com/wikidoc/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/wikidoc/presentation/home/HomeScreen.kt
@@ -237,7 +237,7 @@ fun HomeScreen(
                             .offset {
                                 IntOffset(
                                     (dragState.position.x - 100.dp.toPx()).roundToInt(),
-                                    (dragState.position.y - 60.dp.toPx()).roundToInt()
+                                    (dragState.position.y).roundToInt()
                                 )
                             }
                             .size(width = 200.dp, height = 80.dp)
